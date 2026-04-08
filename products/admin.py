7ae893cfb01db3db76160c721869868b5c67f6ec
stevenmende <stevenmende6@gmail.com>
2026-04-08 10:13:22 +0200

    Modells im Adminpanel registriert

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+# die Modelle werden hier registriert zur Verwaltung im Adminbereich
+
+admin.site.register(Product)
+admin.site.register(Category)
