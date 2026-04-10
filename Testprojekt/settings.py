654859f26fcfd4964998bb0e388aa520e90f37de
stevenmende <stevenmende6@gmail.com>
2026-04-10 11:15:11 +0200

    erste Session Calls und Token Calls erfolgreich

--- Testprojekt/settings.py
+++ Testprojekt/settings.py
@@ -84,6 +84,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [                             # globale Authentifizierungsklassen
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
