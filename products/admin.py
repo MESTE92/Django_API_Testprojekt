98a6fbbb26d4b59f00842454a9fb0f0c34595981
MESTE92 <stevenmende6@gmail.com>
2026-04-13 20:00:20 +0200

    admin.py Jobs registriert

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Jobs
 
 
 # die Modelle werden hier registriert zur Verwaltung im Adminbereich
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Jobs)
